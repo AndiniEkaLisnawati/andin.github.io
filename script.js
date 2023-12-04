685d542ff8214a3e0e30fa80941345ac0a609734
andinandin <148296586+AndiniEkaLisnawati@users.noreply.github.com>
2023-12-04 22:51:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-// input bilangan dan operator
-const operator = prompt('Masukan operator ( salah satu dari: +, -, * dan / )');
-const number1 = parseFloat(prompt('Masukan angka pertama:'));
-const number2 = parseFloat(prompt('Masukan angka ke dua:')); 
+alert('SELAMAT DATANG DI PERMAINAN SUIT JAWA');
+    alert('PERATURAN BERMAIN :                                                                      GAJAH VS ORANG = MENANG GAJAH                                                   ORANG VS SEMUT = MENANG ORANG                                           SEMUT VS GAJAH = MENANG SEMUT');
+let tanya = true;
+while(tanya) {
+  let playerInput = prompt("PILIH : GAJAH, ORANG, SEMUT!!");
+  let player = playerInput.toLowerCase(); // Convert input to lowercase
+  let bot = Math.floor(Math.random()*10)
+      
+  if(bot < 3){
+    bot = 'gajah';
+  }else if(bot > 3 && bot <= 6){
+    bot = 'orang';
+  }else{
+    bot = 'semut';
+  }
+      
+  let hasil;
+      
+  if(player == bot){
+    hasil = 'Hasil permainan kamu seri';
+  }else if(bot == 'semut'){
+    if(player == 'orang'){
+      hasil = 'YEEYYY KAMU MENANG!!';
+    }else if(player == 'gajah'){
+       hasil = 'YAHAHAHA KAMU KALAH';
+    }else{
+      alert("Salah input!")
+    }
+  }else if(bot == 'gajah'){
+    if(player == 'orang'){
+      hasil = 'YAHAHAHA KAMU KALAH';
+    }else if(player == 'semut'){
+      hasil = 'YEEYYY KAMU MENANG!!';
+    }else{
+      alert("Salah input!")
+    }
+  }else if(bot == 'orang'){
+    if(player == 'gajah'){
+      hasil = 'YEEYYY KAMU MENANG!!';
+    }else if(player == 'semut'){
+      hasil = 'YAHAHAHA KAMU KALAH';
+    }else{
+      alert("KAMU SALAH INPUT, COBA CEK LAGI!")
+    }
+  }  
+  alert("kamu milih "+player+", sedangkan Andin memilih "+bot+", makaaaa........... "+hasil);
 
-let result;
-// operasi bilangan
-if (operator == '+') {
-    result = number1 + number2
-}
-if (operator == '-') {
-    result = number1 - number2
-}
-if (operator == '*') {
-    result = number1 * number2
-}
-if (operator == '/') {
-    result = number1 / number2
+  tanya = confirm("MAU MAIN LAGI?");
 }
 
-// hasil operasi bilangan
-document.writeln(`${number1} ${operator} ${number2} = ${result}`);
+alert('TERIMA KASIH SUDAH BERMAIN <3')
