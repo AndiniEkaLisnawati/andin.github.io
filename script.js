7dc89d4d75c101ffbcb4cd2e740a54ffe7062711
andinandin <148296586+AndiniEkaLisnawati@users.noreply.github.com>
2024-02-29 20:25:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-alert('SELAMAT DATANG DI PERMAINAN SUIT JAWA');
-    alert('PERATURAN BERMAIN :                                                                      GAJAH VS ORANG = MENANG GAJAH                                                   ORANG VS SEMUT = MENANG ORANG                                           SEMUT VS GAJAH = MENANG SEMUT');
+alert("SELAMAT DATANG DI PERMAINAN SUIT JAWA");
+alert(
+  "PERATURAN BERMAIN :                                                                      GAJAH VS ORANG = MENANG GAJAH                                                   ORANG VS SEMUT = MENANG ORANG                                           SEMUT VS GAJAH = MENANG SEMUT"
+);
 let tanya = true;
-while(tanya) {
+while (tanya) {
   let playerInput = prompt("PILIH : GAJAH, ORANG, SEMUT!!");
   let player = playerInput.toLowerCase(); // Convert input to lowercase
-  let bot = Math.floor(Math.random()*10)
-      
-  if(bot < 3){
-    bot = 'gajah';
-  }else if(bot > 3 && bot <= 6){
-    bot = 'orang';
-  }else{
-    bot = 'semut';
+  let bot = Math.floor(Math.random() * 10);
+
+  if (bot < 3) {
+    bot = "gajah";
+  } else if (bot > 3 && bot <= 6) {
+    bot = "orang";
+  } else {
+    bot = "semut";
   }
-      
+
   let hasil;
-      
-  if(player == bot){
-    hasil = 'Hasil permainan kamu seri';
-  }else if(bot == 'semut'){
-    if(player == 'orang'){
-      hasil = 'YEEYYY KAMU MENANG!!';
-    }else if(player == 'gajah'){
-       hasil = 'YAHAHAHA KAMU KALAH';
-    }else{
-      alert("Salah input!")
+
+  if (player == bot) {
+    hasil = "KALIAN SERI, SILAHKAN COBA LAGI!";
+  } else if (bot == "semut") {
+    if (player == "orang") {
+      hasil = "YEEYYY KAMU MENANG!!";
+    } else if (player == "gajah") {
+      hasil = "YAHAHAHA KAMU KALAH";
+    } else {
+      alert("Salah input!");
     }
-  }else if(bot == 'gajah'){
-    if(player == 'orang'){
-      hasil = 'YAHAHAHA KAMU KALAH';
-    }else if(player == 'semut'){
-      hasil = 'YEEYYY KAMU MENANG!!';
-    }else{
-      alert("Salah input!")
+  } else if (bot == "gajah") {
+    if (player == "orang") {
+      hasil = "YAHAHAHA KAMU KALAH";
+    } else if (player == "semut") {
+      hasil = "YEEYYY KAMU MENANG!!";
+    } else {
+      alert("Salah input!");
     }
-  }else if(bot == 'orang'){
-    if(player == 'gajah'){
-      hasil = 'YEEYYY KAMU MENANG!!';
-    }else if(player == 'semut'){
-      hasil = 'YAHAHAHA KAMU KALAH';
-    }else{
-      alert("KAMU SALAH INPUT, COBA CEK LAGI!")
+  } else if (bot == "orang") {
+    if (player == "gajah") {
+      hasil = "YEEYYY KAMU MENANG!!";
+    } else if (player == "semut") {
+      hasil = "YAHAHAHA KAMU KALAH";
+    } else {
+      alert("KAMU SALAH INPUT, COBA CEK LAGI!");
     }
-  }  
-  alert("kamu milih "+player+", sedangkan Andin memilih "+bot+", makaaaa........... "+hasil);
+  }
+  alert(
+    "kamu milih " +
+      player +
+      ", sedangkan Clara memilih " +
+      bot +
+      ", makaaaa...... " +
+      hasil
+  );
 
   tanya = confirm("MAU MAIN LAGI?");
 }
 
-alert('TERIMA KASIH SUDAH BERMAIN <3')
+alert("TERIMA KASIH SUDAH BERMAIN <3");
